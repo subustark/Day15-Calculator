9bec2294b8e91d58ea995456c3a0eff0e2303fb9
subustark <113083918+subustark@users.noreply.github.com>
2022-12-20 10:44:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,27 @@
-let one = document.getElementById("1");
-let two = document.getElementById("2");
-let three = document.getElementById("3");
-let four = document.getElementById("4");
-let five = document.getElementById("5");
-let six = document.getElementById("6");
-let seven = document.getElementById("7");
-let eight = document.getElementById("8");
-let nine = document.getElementById("9");
-let zero = document.getElementById("0");
-let add = document.getElementById("+");
-let sub = document.getElementById("-");
-let mul = document.getElementById("*");
-let div = document.getElementById("/");
-let clear = document.getElementById("clear");
-let keypad = document.querySelector(".keypad");
-let equal = document.getElementById("=");
+let display = document.getElementById('display');
 
-keypad.addEventListener("click", display);
-clear.addEventListener("click",clearHistory);
-equal.addEventListener("click",result);
+let buttons = Array.from(document.getElementsByClassName('button'));
 
-
-let input = document.querySelector(".keypad");
-let Result = document.querySelector("#input");
-result.finalResult = "";
-
-document.body.addEventListener("keypress",keypress);
-
-function keypress(x){
-    let history= document.querySelector(".history");
-    if((x.key<10 && x.key>=0) || x.key==="*" || x.key==="-" || x.key==="+" || x.key==="/" ){
-        history.innerText += x.key;
-    }else if(x.key==="Enter"){
-        result();
-    }
-}
-
-function display(x){
-    let history= document.querySelector(".history");
-    if(x.target.value===undefined){
-        history.innerHTML = history.innerHTML+"";
-    }else{
-        history.innerHTML = history.innerHTML+x.target.value;
-    }
-    
-}
-
-function clearHistory(){
-    let history= document.querySelector(".history");
-    let Result = document.querySelector("#input");
-    history.innerHTML="";
-    Result.value = "0";
-}
-
-
-function result(){
-    let history= document.querySelector(".history"); 
-    let Result = document.querySelector("#input");
-    Result.value = eval(history.innerText); 
-}
-
-function calculation(num1,operator,num2){
-    if(operator === "+"){
-        return num1+num2;
-    }else if(operator === "-"){
-        return num1-num2;    
-    }else if(operator === "*"){
-        return num1*num2;
-    }else if(operator === "/"){
-        return num1/num2;
-    }
-}
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
